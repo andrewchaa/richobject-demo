84869776cd0e09e94a0cda4791f27c39f3574e4c
Andrew Chaa <andrew.yh.chaa@gmail.com>
2019-04-23 09:13:53 +0100

    leaking dtos

---
+++ RichObject.Domain/ICustomerRepositoryV3.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RichObject.Domain
+{
+    public interface ICustomerRepositoryV3
+    {
+        Task<CustomerDataV2> Get(Guid customerId);
+    }
+    
+    public class CustomerDataV2
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string IdDocumentType { get; set; }
+        public string IdDocumentNumber { get; set; }
+        public IEnumerable<AddressDataV2> Addresses { get; set; }
+    }
+    
+    public class AddressDataV2
+    {
+        public bool CurrentAddress { get; set; }
+        public string HouseNoOrName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string PostCode { get; set; }
+    }
+
+}
