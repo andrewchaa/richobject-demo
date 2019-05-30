9fa4274f144ceb62b3ad808e7ba1a0283af8358c
andrewchaa <andrew.chaa@clear.bank>
2019-05-30 08:42:34 +0100

    cb

--- rich-object-cb/RichObject.Domain/Repositories/ICustomerRepository4.cs
+++ rich-object-cb/RichObject.Domain/Repositories/ICustomerRepository4.cs
@@ -7,6 +7,7 @@ namespace RichObject.Domain.Repositories
     public interface ICustomerRepository4
     {
         Task<Customer4> Get(Guid customerId);
+        Task<Customer4> Get(Guid customerId, IAddressRepository4 addressRepository);
         Task<Guid> Insert(Customer4 customerData);
     }
 }
